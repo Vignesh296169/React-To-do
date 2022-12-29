3d7d8b00619e8d0e8ce32a8afce368aa661fc57e
vignesh <vignesh296169@gmail.com>
2022-12-29 18:31:36 +0530

    name

--- src/components/Element.js
+++ src/components/Element.js
@@ -8,7 +8,7 @@ function Element(props) {
    const [firstonem,secondone]=useState(props.title)
   return (
     <div className='first'>
-    <div>{firstonem}{firstonem==='pavitha'?<h6 style={{color:"red"}}>Sorry,Name not Found instead of this try name Pavithra </h6>:'' || firstonem==="pavithra" ?<h2>vanakam-Chef😍</h2>:''}</div>
+    <div>{firstonem}{firstonem=== 'pavitha'?<h6 style={{color:"red"}}>Sorry,Name not Found instead of this try name Pavithra </h6>:'' || firstonem==="pavithra" ?<h2>vanakam-Chef😍</h2>:''}</div>
     <div className='second'>
      <button className={firstonem.length===0?"result":<p>''</p>}  onClick={deleteHandler}>delete <i class="fa-solid fa-ban"></i></button>
      </div>
